5214f4cfc6b3cac1feadd4eeb54e36706d9c8b85
Nicolas Tuller <tullern85@hotmail.fr>
2021-11-05 13:43:41 +0100

    minor

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -17,8 +17,8 @@ require('dotenv').config();
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
-    const userId = decodedToken.userId;
+    const verifyToken = jwt.verify(token, process.env.SECRET_TOKEN);
+    const userId = verifyToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
